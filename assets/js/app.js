c1528228f7ff78b3b17ba0780d99a9671b901a7b
Brian Akpa <bakpa79@gmail.com>
2014-01-05 20:12:49 -0500

    Files added
    Setup repo

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+// LIBS
+// @codekit-prepend "libs/jquery.min.js";
+// @codekit-prepend "libs/jquery.min.map";
+// @codekit-prepend "libs/jquery.autocomplete.js";
+// @codekit-prepend "libs/jquery.cookie.js";
+// @codekit-prepend "libs/jquery-migrate.min.js";
+// @codekit-prepend "libs/modernizr.js";
+// @codekit-prepend "libs/foundation.min.js";
+// @codekit-prepend "libs/foundation.abide.js";
+// @codekit-prepend "libs/foundation.accordion.js";
+// @codekit-prepend "libs/foundation.alert.js";
+// @codekit-prepend "libs/foundation.clearing.js";
+// @codekit-prepend "libs/foundation.dropdown.js";
+// @codekit-prepend "libs/foundation.interchange.js";
+// @codekit-prepend "libs/foundation.joyride.js";
+// @codekit-prepend "libs/foundation.magellan.js";
+// @codekit-prepend "libs/foundation.offcanvas.js";
+// @codekit-prepend "libs/foundation.orbit.js";
+// @codekit-prepend "libs/foundation.reveal.js";
+// @codekit-prepend "libs/foundation.tab.js";
+// @codekit-prepend "libs/foundation.tooltip.js";
+// @codekit-prepend "libs/foundation.topbar.js";
+// @codekit-prepend "libs/webfonts.js";
+
+
+// FUNCTIONS
+// @codekit-prepend "functions/core.js";
